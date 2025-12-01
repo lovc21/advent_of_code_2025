db42cd7aa2f047cedd4eaf455dffcea033c5a4ee
Jakob <jakob.dekleva@gmail.com>
2025-12-01 23:47:01 +0100

    Chore: part one

--- src/day01.zig
+++ src/day01.zig
@@ -10,22 +10,56 @@ const data = @embedFile("data/day01.txt");
 
 pub fn main() !void {
     var buffer: [8]i32 = undefined;
-    const lint = List(i32).initBuffer(&buffer);
-    _ = lint;
+    const list = List(i32).initBuffer(&buffer);
+    _ = list;
+
+    const starting_number: i32 = 50;
+    var sum: i32 = 0;
+    var count: i32 = 0;
+    var index: i32 = 0;
     var lins = splitAny(u8, data, "\n");
     while (lins.next()) |line| {
         const trimmed_line = trim(u8, line, " ");
         if (trimmed_line.len == 0) continue;
-        var token = tokenizeAny(u8, trimmed_line, "\t");
-        while (token.next()) |token_line| {
-            const first_letters = try parseFloat(f32, token_line);
-            if (first_letters == "L") {
-                print("{s}\n", .{trimmed_line});
+        const first_letters = trimmed_line[0];
+        var number = try parseInt(i32, trimmed_line[1..], 10);
+
+        print("first_letters: {}\n", .{first_letters});
+        print("number {}\n", .{number});
+
+        // edge case number can be bigger than 100
+        if (number > 100) {
+            const div = @divTrunc(number, 100);
+            number = number - div * 100;
+        }
+
+        if (index == 0) {
+            if (first_letters == 76) {
+                sum = starting_number - number;
             } else {
-                print("{s}\n", .{trimmed_line});
+                sum = starting_number + number;
+            }
+        } else {
+            if (first_letters == 76) {
+                sum -= number;
+            } else {
+                sum += number;
             }
         }
+
+        if (sum == 100 or sum == 0) {
+            count += 1;
+        } else if (sum < 0) {
+            sum = sum + 100;
+        } else if (sum > 100) {
+            sum = sum - 100;
+        }
+
+        index += 1;
+        print("-----index: {}----\n", .{index});
+        print("sum: {}\n", .{sum});
     }
+    print("count: {}\n", .{count});
 }
 
 // Useful stdlib functions
