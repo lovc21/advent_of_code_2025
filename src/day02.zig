fd15484ba1fde5f87bbaa763e30aa25ceb8e6838
Jakob <jakob.dekleva@gmail.com>
2025-12-02 22:19:38 +0100

    Chore: day2 done

--- src/day02.zig
+++ src/day02.zig
@@ -8,19 +8,79 @@ const print = std.debug.print;
 
 const data = @embedFile("data/day02.txt");
 
-pub fn main() !void {
-    var buffer: [8]i32 = undefined;
-    const list = List(i32).initBuffer(&buffer);
-    _ = list;
+// build whit zig build day02 --release=fast -Dcpu=znver1 -Dtarget=x86_64-linux if not its slow as fuck
 
+pub fn main() !void {
+    var count: usize = 0;
     var lins = tokenizeAny(u8, data, ",\n");
     while (lins.next()) |line| {
         var range = tokenizeAny(u8, line, "-");
-        const start = try parseInt(i32, range.next().?, 10);
-        const end = try parseInt(i32, range.next().?, 10);
-        print("start: {}\n", .{start});
-        print("end: {}\n", .{end});
+        const start = try parseInt(usize, range.next().?, 10);
+        const end = try parseInt(usize, range.next().?, 10);
+        for (start..end + 1) |i| {
+            const max_len = 10000000;
+            var buf: [max_len]u8 = undefined;
+            const numAsString = try std.fmt.bufPrint(&buf, "{}", .{i});
+
+            // part 1
+            // check if all chars are the same
+            // var allSame = true;
+            // for (numAsString) |c| {
+            //     if (numAsString[0] != c) {
+            //         allSame = false;
+            //         break;
+            //     }
+            // }
+            //
+            // if (allSame) {
+            //     print(" napacen ID je {s}\n", .{numAsString});
+            //     count += try parseInt(usize, numAsString, 10);
+            //     continue;
+            // }
+            // else if (numAsString.len % 2 == 0) {
+            //     // chek for repeated chars in the middle
+            //     const stringlen: usize = numAsString.len / 2;
+            //     const firstpart = numAsString[0..stringlen];
+            //     const secondpart = numAsString[stringlen..];
+            //     if (std.mem.eql(u8, firstpart, secondpart)) {
+            //         count += try parseInt(usize, numAsString, 10);
+            //         print(" napacen ID je {s}\n", .{numAsString});
+            //     }
+            // } else {
+            //
+            // }
+            //
+
+            // part 2
+            for (2..numAsString.len + 1) |j| {
+                if (numAsString.len % j != 0) { // 10 % 2 != 0 False
+                    continue;
+                }
+
+                var thesame = true;
+                const chunk_size = numAsString.len / j; // 10 / 2 = 5
+                const prvi = numAsString[0..chunk_size]; // 0 do 5 = 21212
+                for (0..numAsString.len / chunk_size) |c| { // 0 1
+                    const chunk = numAsString[c * chunk_size .. c * chunk_size + chunk_size]; // 21212
+                    // print("MY moving chunk is {s}\n", .{chunk});
+                    if (std.mem.eql(u8, prvi, chunk)) {} else {
+                        thesame = false;
+                        break;
+                    }
+                }
+                if (thesame) {
+                    count += try parseInt(usize, numAsString, 10);
+                    // print(" napacen ID je {s}\n", .{numAsString});
+                    break;
+                }
+            }
+            // print(" number je {s}\n", .{numAsString});
+        }
+
+        // print("start: {}\n", .{start});
+        // print("end: {}\n", .{end});
     }
+    print("count je {d} \n", .{count});
 }
 
 // Useful stdlib functions
